51ca333c9355d550657884aee9b17f7de9bc39a1
MariaIlyasMalik <141207866+MariaIlyasMalik@users.noreply.github.com>
2023-11-15 08:33:31 +0000

    code added

--- host_check.py
+++ host_check.py
@@ -1,1 +1,13 @@
-# Your Code here
+text = input()
+
+def extract_host(text):
+    atpos = text.find('@')
+    if atpos != -1:
+        sppos = text.find(' ', atpos)
+        if sppos != -1:
+            host = text[atpos + 1 : sppos]
+            return host
+    return None
+
+result = extract_host(text)
+print(result)
